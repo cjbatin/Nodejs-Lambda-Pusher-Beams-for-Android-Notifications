2b3f0a469cb59f484aed489b492e9fc6f3df7c78
Christopher Batin <chris@batin.me>
2019-04-12 20:45:06 +0100

    Init commit

---
+++ NodeJS/index.js
@@ -0,0 +1,19 @@
+exports.handler = function(event, context, callback) {
+    const PushNotifications = require('@pusher/push-notifications-server');
+    let beamsClient = new PushNotifications({
+      instanceId: 'YOUR_INSTANCE_ID',
+      secretKey: 'YOUR_SECRET_KEY'
+    });
+    beamsClient.publishToInterests(['hello'], {
+          fcm: {
+            notification: {
+              "title": event.title,
+              "body": event.message
+            }
+          }
+    }).then((publishResponse) => {
+       callback(null, 'Just published:' + publishResponse.publishId);
+    }).catch((error) => {
+      callback(error);
+    });
+};
